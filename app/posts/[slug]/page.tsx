c8812c9f456b50dd2cf3e7d3ff01431c9be96526
Estelle Troadec <estelle.t29@gmail.com>
2024-11-25 14:14:23 +0100

    feat: create a '404 error - not found' page if post does not exist

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,12 @@
 import { getPost } from "@/lib/posts";
+import { notFound } from "next/navigation";
 
 export default async function RoutePage(props: {params: {slug: string}}) {
     const post = await getPost(props.params.slug);
+
+    if (!post) {
+        notFound();
+    }
 
     return(
         <div>
