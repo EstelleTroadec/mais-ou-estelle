ec215049c09c19ca185bdd5923882d2c3bf78edd
Estelle Troadec <estelle.t29@gmail.com>
2024-11-26 10:54:56 +0100

    update: add a calendar icon next to the date

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import {
   Card,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -21,12 +20,18 @@ export default async function Home() {
       {posts.map((post) => (
         <Card key={post.slug}>
           <CardHeader>
-          <CardImage src={post.mainImage} alt={post.title} />
-          <p className="text-sm text-muted-foreground">
-            {dayjs(post.publishedAt).format('D MMMM YYYY')}
-          </p>
+            <CardImage src={post.mainImage} alt={post.title} />
+            <div className="items-center gap-2">
+              <div className="my-4 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mr-2.5 mt-0.5 size-3.5 text-muted-foreground">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                </svg>
+                <p className="text-sm text-muted-foreground">
+                  {dayjs(post.publishedAt).format('D MMMM YYYY')}
+                </p>
+              </div>
+            </div>
             <CardTitle>{post.title}</CardTitle>
-            <CardDescription>{post.description}</CardDescription>
           </CardHeader>
           <CardFooter>
             <Link
