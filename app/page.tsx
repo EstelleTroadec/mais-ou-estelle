aaaa8de8848cd10128c9a8cc29a2bea3fa7a3d06
Estelle Troadec <estelle.t29@gmail.com>
2024-11-26 11:35:21 +0100

    update: use grid to make the posts appear by 2 in each row in the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -16,34 +16,34 @@ dayjs.locale('fr');
 export default async function Home() {
   const posts = await getPosts();
   return (
-    <div className="space-y-4">
-      {posts.map((post) => (
-        <Card key={post.slug}>
-          <CardHeader>
-            <CardImage src={post.mainImage} alt={post.title} />
-            <div className="items-center gap-2">
-              <div className="my-4 flex">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mr-2.5 mt-0.5 size-3.5 text-muted-foreground">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
-                </svg>
-                <p className="text-sm text-muted-foreground">
-                  {dayjs(post.publishedAt).format('D MMMM YYYY')}
-                </p>
+    <div className="m-auto w-3/4 space-y-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        {posts.map((post) => (
+          <Card key={post.slug} className="flex flex-col gap-2">
+            <CardHeader>
+                <CardImage src={post.mainImage} alt={post.title} />
+                <div className="items-center gap-2">
+                <div className="my-4 flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mr-2.5 mt-0.5 size-3.5 text-muted-foreground">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                  </svg>
+                  <p className="text-sm text-muted-foreground">
+                    {dayjs(post.publishedAt).format('D MMMM YYYY')}
+                  </p>
+                </div>
               </div>
-            </div>
-            <CardTitle>{post.title}</CardTitle>
-          </CardHeader>
-          <CardFooter>
-            <Link
-              className="text-blue-500 hover:underline"
-              href={`/posts/${post.slug}`}
-            >
-              Voir plus →
-            </Link>
-            <LikeCount slug={post.slug} /> 
-          </CardFooter>
-        </Card>
-      ))}
+              <Link href={`/posts/${post.slug}`}>
+                <CardTitle className="text-footerBg">
+                  {post.title}
+                </CardTitle>
+              </Link>
+            </CardHeader>
+            <CardFooter className="flex flex-col items-start text-muted-foreground">
+              <LikeCount slug={post.slug} />
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
