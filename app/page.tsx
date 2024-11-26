ab77019165372392099ffb99571075039559567e
Estelle Troadec <estelle.t29@gmail.com>
2024-11-26 20:13:27 +0100

    update: add background photo to the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/card";
 import { getPosts } from "@/lib/posts";
 import Link from "next/link";
+import "../styles/globals.css";
 import { LikeCount } from "../src/components/likesCount/LikeCount";
 import dayjs from 'dayjs';
 import 'dayjs/locale/fr';
@@ -23,34 +24,37 @@ export default async function Home() {
   const visiblePosts = posts.slice(0, 6);
 
   return (
-    <div className="m-auto w-3/4 space-y-4">
-      <h1 className="ml-3 mt-12 font-poppins text-4xl font-semibold text-title">Mes derniers articles</h1>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        {visiblePosts.map((post) => (
-          <Card key={post.slug} className="flex flex-col gap-2">
-            <CardHeader>
-              <CardImage src={post.mainImage} alt={post.title} />
-              <div className="items-center gap-2">
-                <div className="my-4 flex">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mr-2.5 mt-0.5 size-3.5 text-muted-foreground">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
-                  </svg>
-                  <p className="text-sm text-muted-foreground">
-                    {dayjs(post.publishedAt).format('D MMMM YYYY')}
-                  </p>
+    <div className="flex flex-col">
+      <div className="bg-homePage h-[40rem] w-full" />
+      <div className="m-auto w-3/4 space-y-4">
+        <h1 className="ml-3 mt-12 font-poppins text-4xl font-semibold text-title">Mes derniers articles</h1>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          {visiblePosts.map((post) => (
+            <Card key={post.slug} className="flex flex-col gap-2">
+              <CardHeader>
+                <CardImage src={post.mainImage} alt={post.title} />
+                <div className="items-center gap-2">
+                  <div className="my-4 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mr-2.5 mt-0.5 size-3.5 text-muted-foreground">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                    </svg>
+                    <p className="text-sm text-muted-foreground">
+                      {dayjs(post.publishedAt).format('D MMMM YYYY')}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <Link href={`/posts/${post.slug}`}>
-                <CardTitle className="text-footerBg">
-                  {post.title}
-                </CardTitle>
-              </Link>
-            </CardHeader>
-            <CardFooter className="flex flex-col items-start text-muted-foreground">
-              <LikeCount slug={post.slug} />
-            </CardFooter>
-          </Card>
-        ))}
+                <Link href={`/posts/${post.slug}`}>
+                  <CardTitle className="text-footerBg">
+                    {post.title}
+                  </CardTitle>
+                </Link>
+              </CardHeader>
+              <CardFooter className="flex flex-col items-start text-muted-foreground">
+                <LikeCount slug={post.slug} />
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
