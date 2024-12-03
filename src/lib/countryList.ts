555a1e2da1724e59ed2ca18d05890e3186a75e21
Estelle Troadec <estelle.t29@gmail.com>
2024-12-03 11:52:02 +0100

    feat: dynamise header menu (only with a manual array for the moment)

---
+++ src/lib/countryList.ts
@@ -0,0 +1,27 @@
+const continentCountryMap = {
+  "Amérique du Nord": [
+    { name: "États-Unis", link: "/countries/etats-unis" },
+    { name: "Mexique", link: "/countries/mexique" },
+/*     { name: "Canada", link: "/countries/canada" } */
+  ],
+/*   "Amérique du Sud": [
+    { name: "Brésil", link: "/countries/bresil" },
+    { name: "Argentine", link: "/countries/argentine" },
+    { name: "Chili", link: "/countries/chili" }
+  ], */
+/*   "Europe": [
+    { name: "France", link: "/countries/france" },
+    { name: "Espagne", link: "/countries/espagne" }
+  ], */
+/*   "Asie": [
+    { name: "Thailande", link: "/countries/thailande" },
+    { name: "Singapour", link: "/countries/singapour" },
+    { name: "Malaisie", link: "/countries/malaisie" },
+    { name: "Bali", link: "/countries/bali" }
+  ], */
+  "Océanie": [
+    { name: "Polynésie Française", link: "/countries/polynesie-francaise" }
+  ],
+};
+
+export { continentCountryMap };
