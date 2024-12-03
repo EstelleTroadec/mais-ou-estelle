e63cc0c2faf0ffecf83501f7846b1451ccfc4c92
Estelle Troadec <estelle.t29@gmail.com>
2024-12-03 20:07:56 +0100

    update: apply the same style for posts in home page as the ones in country page

--- app/countries/[slug]/page.tsx
+++ app/countries/[slug]/page.tsx
@@ -90,33 +90,33 @@ export default async function CountryPage(props: { params: Promise<{ slug: strin
         </div>
       </div>
       <div className="m-auto w-4/5 space-y-4 md:mx-auto md:w-[90%] lg:mx-auto">
-      <h1 className="ml-1 mt-12 font-poppins text-xl font-semibold text-title md:ml-3 md:text-3xl">Tous mes articles sur {countryPage.article} {countryPage.name} </h1>
+        <h1 className="ml-1 mt-12 font-poppins text-xl font-semibold text-title md:ml-3 md:text-3xl">Tous mes articles sur {countryPage.article} {countryPage.name} </h1>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:justify-center">
           {countryPosts?.posts?.map((post) => (
-              <Card key={post.slug} className="flex flex-col gap-2">
-                <CardHeader>
-                    <CardImage src={post.mainImage} alt={post.title} />
-                    <div className="items-center gap-2">
-                    <div className="my-4 flex">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mr-2.5 mt-0.5 size-3.5 text-muted-foreground">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
-                      </svg>
-                      <p className="text-sm text-muted-foreground">
-                        {dayjs(post.publishedAt).format('D MMMM YYYY')}
-                      </p>
-                    </div>
+            <Card key={post.slug} className="flex flex-col gap-2">
+              <CardHeader>
+                  <CardImage className="h-80" src={post.mainImage} alt={post.title} />
+                  <div className="items-center gap-2">
+                  <div className="my-4 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mr-2.5 mt-0.5 size-3.5 text-muted-foreground">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                    </svg>
+                    <p className="text-sm text-muted-foreground">
+                      {dayjs(post.publishedAt).format('D MMMM YYYY')}
+                    </p>
                   </div>
-                  <Link href={`/posts/${post.slug}`}>
-                    <CardTitle className="text-xl text-footerBg lg:text-3xl">
-                      {post.title}
-                    </CardTitle>
-                  </Link>
-                </CardHeader>
-                <CardFooter className="flex flex-col items-start text-muted-foreground">
-                  <LikeCount slug={post.slug} />
-                </CardFooter>
-              </Card>
-            ))}
+                </div>
+                <Link href={`/posts/${post.slug}`}>
+                  <CardTitle className="text-xl text-footerBg lg:text-3xl">
+                    {post.title}
+                  </CardTitle>
+                </Link>
+              </CardHeader>
+              <CardFooter className="flex flex-col items-start text-muted-foreground">
+                <LikeCount slug={post.slug} />
+              </CardFooter>
+            </Card>
+          ))}
         </div>
       </div>
     </div>
