b390927cefd89a4d2c6ffe25aba367b114628cc5
Estelle Troadec <estelle.t29@gmail.com>
2024-12-04 14:07:26 +0100

    update: add style to the subtitle in posts

--- src/features/mdx/MdxStyle.tsx
+++ src/features/mdx/MdxStyle.tsx
@@ -7,3 +7,7 @@ export type MdxColorProps = {
 export const MdxTitle = (props: MdxColorProps) => {
   return <h2 className="font-poppins uppercase text-title">{props.children}</h2>;
 };
+
+export const MdxSubTitle = (props: MdxColorProps) => {
+  return <h3 className="font-poppins uppercase text-subtitle">{props.children}</h3>;
+};
